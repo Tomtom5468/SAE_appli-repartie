defaffe54a77699550d08c01ea4dbe032eab65e1
KerrianG <kerrian.gesnel2@etu.univ-lorraine.fr>
2024-06-11 11:25:04 +0200

    Modification de la classe Restaurant.java

--- src/classes/Restaurant.java
+++ src/classes/Restaurant.java
@@ -4,5 +4,26 @@ import java.io.Serializable;
 
 public class Restaurant implements Serializable {
 
-    private
+    private int id;
+    private String nom;
+    private String adresse;
+    private double latitude;
+    private double longitude;
+
+    /**
+     * Constructeur de la classe Restaurant
+     * @param id
+     * @param nom
+     * @param adresse
+     * @param latitude
+     * @param longitude
+     * @return void
+     */
+    public Restaurant(int id, String nom, String adresse, double latitude, double longitude) {
+        this.id = id;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
