b466798f0b56af7e01bcf59ca03a2f199faeabda
KerrianG <kerrian.gesnel2@etu.univ-lorraine.fr>
2024-06-11 14:14:35 +0200

    Ajout de la gestion des exception dans les lancements de service RMI

--- src/M1/LancerM1.java
+++ src/M1/LancerM1.java
@@ -1,15 +1,21 @@
+import java.rmi.AccessException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class LancerM1 {
     public static void main(String[] args) {
-        try{
+        try {
             ServiceRestaurant sr = new ServiceRestaurant();
             ServiceRestaurantInterface sri = (ServiceRestaurantInterface) UnicastRemoteObject.exportObject(sr, 54680);
 
             Registry reg = LocateRegistry.createRegistry(54680);
             reg.rebind("Restaurants", sri);
+        }catch(RemoteException e) {
+            System.out.println("Erreur de création du serveur " + e.getMessage());
+        }catch(AccessException e) {
+            System.out.println("Erreur d'accès au serveur " + e.getMessage());
         }catch (Exception e){
             System.out.println("Erreur de connexion " + e.getMessage());
         }
--- src/M2/LancerM2.java
+++ src/M2/LancerM2.java
@@ -1,3 +1,5 @@
+import java.rmi.AccessException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
@@ -9,9 +11,13 @@ public class LancerM2 {
             ServiceVelibInterface svi = (ServiceVelibInterface) UnicastRemoteObject.exportObject(sv, 54600);
             Registry reg = LocateRegistry.createRegistry(1099);
             reg.rebind("M2", svi);
+        } catch (RemoteException e) {
+            System.out.println("Erreur de création du serveur " + e.getMessage());
+        } catch (AccessException e) {
+            System.out.println("Erreur d'accès au serveur " + e.getMessage());
         } catch (Exception e) {
-            System.out.println("Serveur exception: " + e.toString());
-            e.printStackTrace();
+            System.out.println("Erreur de connexion " + e.getMessage());
         }
+
     }
 }
