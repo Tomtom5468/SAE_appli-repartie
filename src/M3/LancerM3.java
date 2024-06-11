8eb6ba4783edf43e1f5bfc334caa9b357c02c6e4
Thomas M <micheli17u@etu.univ-lorraine.fr>
2024-06-11 14:31:07 +0200

    .class for test M1 and M3

--- src/M3/LancerM3.java
+++ src/M3/LancerM3.java
@@ -1,2 +1,15 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class LancerM3 {
+    public static void main(String[] args) {
+        try{
+            Registry reg = LocateRegistry.getRegistry("localhost");
+            ServiceRestaurantInterface service = (ServiceRestaurantInterface) reg.lookup("Restaurants");
+    
+            System.out.println(service.getRestaurants().size());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
