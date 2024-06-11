1216b0e579b1a97257606f5a21dbb86f0d5a0257
KerrianG <kerrian.gesnel2@etu.univ-lorraine.fr>
2024-06-11 14:33:05 +0200

    Ajout de la méthode addRestaurant au serviceRestaurant (M1) et gestion transactionnelle de la BD

--- src/M1/ServiceRestaurantInterface.java
+++ src/M1/ServiceRestaurantInterface.java
@@ -4,4 +4,5 @@ import java.util.List;
 public interface ServiceRestaurantInterface extends Remote {
     public List<Restaurant> getRestaurants();
     public Restaurant getRestaurantById(int id);
+    public boolean addRestaurant(Restaurant restaurant);
 }
